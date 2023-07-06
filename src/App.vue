efccacc86c2bf0bf5d741097fea32f79e27d216a
dragos damjanovic <dragos@infopuls.net>
2023-07-06 13:46:05 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+    <navbar></navbar>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+    <router-view></router-view>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    <!-- <page-viewer
+    v-if="pages.length > 0"
+    :page="pages[activePage]"
+    ></page-viewer>
+
+    <create-page
+    @page-created="pageCreated"
+    ></create-page> -->
+  </template>
+  
+  <script>
+  import Navbar from './components/Navbar.vue';
+
+  export default {
+    components: {
+        Navbar,
+    },
+    methods: {
+      pageCreated(pageObj) {
+        this.pages.push(pageObj)
+      }
+    }
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  </script>
+  
